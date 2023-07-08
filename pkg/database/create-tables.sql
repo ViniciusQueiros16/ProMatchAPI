feae53cb54cb323c283fe0f6cc67f93898b85c6f
Vinicius <vinny.queiyros16@gmail.com>
2023-07-08 12:35:32 -0300

    fit: Added username,updated_at and deleted_at coluns in users table

--- pkg/database/create-tables.sql
+++ pkg/database/create-tables.sql
@@ -1,21 +1,24 @@
+DROP TABLE IF EXISTS auth_tokens;
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100) UNIQUE,
     name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE,
     password VARCHAR(100) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
 );
 
-INSERT INTO users (name, email, password) VALUES
-    ('Silvio', 'silvio@example.com', 'senha1'),
-    ('Tony', 'tony@example.com', 'senha2'),
-    ('Luciana', 'luciana@example.com', 'senha3'),
-    ('Samara', 'samara@example.com', 'senha4');
+INSERT INTO users (username, name, email, password) VALUES
+    ('silsil','Silvio', 'silvio@example.com', 'senha1'),
+    ('Tony','Antônio', 'tony@example.com', 'senha2'),
+    ('Lu','Luciana', 'luciana@example.com', 'senha3'),
+    ('Sami','Samara', 'samara@example.com', 'senha4');
 
 
 
-DROP TABLE IF EXISTS auth_tokens;
 CREATE TABLE auth_tokens (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
