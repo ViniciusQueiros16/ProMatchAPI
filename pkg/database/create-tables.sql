bc2935e6bcd96c88b85a2154ab64f585ac647138
Vinicius <vinny.queiyros16@gmail.com>
2023-08-01 19:38:54 -0300

    Created profile table

--- pkg/database/create-tables.sql
+++ pkg/database/create-tables.sql
@@ -27,3 +27,25 @@ CREATE TABLE auth_tokens (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+
+CREATE TABLE profile (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    id_user INT NOT NULL,
+    avatar VARCHAR(255),
+    birthdate DATE,
+    company VARCHAR(100),
+    gender VARCHAR(10),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP,
+    FOREIGN KEY (id_user) REFERENCES users (id) ON DELETE CASCADE
+);
+
+
+INSERT INTO profile (id_user, avatar, birthdate, company, gender, created_at)
+VALUES
+    (1, 'https://avatars.githubusercontent.com/ViniciusQueiros16', '1990-06-15', 'ABC Company', 'Male', NOW()),
+    (2, 'https://avatars.githubusercontent.com/ViniciusQueiros16', '1985-12-25', 'XYZ Corporation', 'Female', NOW()),
+    (3, 'https://avatars.githubusercontent.com/ViniciusQueiros16', '1998-03-10', 'Sample Corp', 'Other', NOW());
+
