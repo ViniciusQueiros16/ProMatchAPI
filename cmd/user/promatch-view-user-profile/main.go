128e1b8a257ca9450df5b122123fd6636b07753a
Vinicius <vinny.queiyros16@gmail.com>
2023-08-03 19:13:32 -0300

    Fetch user profile by username

--- cmd/user/promatch-view-user-profile/main.go
+++ cmd/user/promatch-view-user-profile/main.go
@@ -5,7 +5,6 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
-	"strconv"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -15,45 +14,35 @@ import (
 	"github.com/promatch/structs"
 )
 
-func FetchUserProfile(db *sql.DB, id int64) ([]structs.UserProfile, error) {
-	var userProfiles []structs.UserProfile
+func FetchUserProfile(db *sql.DB, username string) (structs.UserProfile, error) {
+	var userProfile structs.UserProfile
 
 	query := `
 		SELECT u.id, u.name, u.username, u.email, p.avatar, p.birthdate, p.company, p.gender
 		FROM users u
 		LEFT JOIN profile p ON u.id = p.id_user
-		WHERE u.id = ?;
+		WHERE u.username = ? LIMIT 1;
 	`
 
-	rows, err := db.Query(query, id)
-	if err != nil {
-		return nil, fmt.Errorf("FetchUserProfile: %w", err)
-	}
-	defer rows.Close()
+	row := db.QueryRow(query, username)
 
-	for rows.Next() {
-		var userProfile structs.UserProfile
-		var birthdate sql.NullTime
+	var birthdate sql.NullTime
 
-		if err := rows.Scan(
-			&userProfile.UserID, &userProfile.Name, &userProfile.Username, &userProfile.Email,
-			&userProfile.Avatar, &birthdate, &userProfile.Company, &userProfile.Gender,
-		); err != nil {
-			return nil, fmt.Errorf("FetchUserProfile: %w", err)
+	if err := row.Scan(
+		&userProfile.UserID, &userProfile.Name, &userProfile.Username, &userProfile.Email,
+		&userProfile.Avatar, &birthdate, &userProfile.Company, &userProfile.Gender,
+	); err != nil {
+		if err == sql.ErrNoRows {
+			return structs.UserProfile{}, nil
 		}
-
-		if birthdate.Valid {
-			userProfile.Birthdate = birthdate.Time
-		}
-
-		userProfiles = append(userProfiles, userProfile)
+		return structs.UserProfile{}, fmt.Errorf("FetchUserProfile: %w", err)
 	}
 
-	if err := rows.Err(); err != nil {
-		return nil, fmt.Errorf("FetchUserProfile: %w", err)
+	if birthdate.Valid {
+		userProfile.Birthdate = birthdate.Time
 	}
 
-	return userProfiles, nil
+	return userProfile, nil
 }
 
 func handler(ctc context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
@@ -65,22 +54,16 @@ func handler(ctc context.Context, request events.APIGatewayProxyRequest) (events
 	}
 	defer database.CloseDB()
 
-	userIDStr := request.QueryStringParameters["userID"]
-	userID, err := strconv.ParseInt(userIDStr, 10, 64)
-	if err != nil {
-		return response.ApiResponse(http.StatusBadRequest, structs.ErrorBody{
-			ErrorMsg: aws.String("Invalid userID format: must be an integer"),
-		})
-	}
+	username := request.QueryStringParameters["username"]
 
-	result, err := FetchUserProfile(db, userID)
+	result, err := FetchUserProfile(db, username)
 	if err != nil {
 		return response.ApiResponse(http.StatusBadRequest, structs.ErrorBody{
 			ErrorMsg: aws.String(err.Error()),
 		})
 	}
 
-	fmt.Printf("User Profile found: %v\n", result)
+	fmt.Printf("User Profile found: %+v\n", result)
 
 	return response.ApiResponse(http.StatusOK, result)
 }
