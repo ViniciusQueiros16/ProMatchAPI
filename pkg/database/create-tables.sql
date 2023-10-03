10702eebcc670d3a9c0a52f7dd5bb01a14ed8e6a
Vinicius <vinny.queiyros16@gmail.com>
2023-10-03 16:50:42 -0300

    Created  matches and user_types table and added columns user_type_id, verified and about

--- pkg/database/create-tables.sql
+++ pkg/database/create-tables.sql
@@ -1,36 +1,54 @@
 -- DROP TABLE IF EXISTS auth_tokens;
 -- DROP TABLE IF EXISTS profile;
 -- DROP TABLE IF EXISTS posts;
+-- DROP TABLE IF EXISTS matches;
 -- DROP TABLE IF EXISTS users;
+-- DROP TABLE IF EXISTS user_types;
+
+-- Create user_types table
+CREATE TABLE user_types (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    type_name VARCHAR(100) UNIQUE
+);
+
+-- Insert user types
+INSERT INTO user_types (type_name) VALUES
+    ('client'),
+    ('professional');
+
+-- Create users table
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(100) UNIQUE,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE,
     password VARCHAR(100) NOT NULL,
+    user_type_id INT NOT NULL,
+    verified BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP,
-    deleted_at TIMESTAMP
+    deleted_at TIMESTAMP,
+    FOREIGN KEY (user_type_id) REFERENCES user_types(id)
 );
 
-INSERT INTO users (username, name, email, password) VALUES
-    ('silsil','Silvio', 'silvio@example.com', '$2a$10$8yaLfN1mVcQZcMis149VZesprNumE0ULCaxKvM2P8mXuZ5eWTkzVG'),
-    ('Tony','Antônio', 'tony@example.com', '$2a$10$8yaLfN1mVcQZcMis149VZesprNumE0ULCaxKvM2P8mXuZ5eWTkzVG'),
-    ('Lu','Luciana', 'luciana@example.com', '$2a$10$8yaLfN1mVcQZcMis149VZesprNumE0ULCaxKvM2P8mXuZ5eWTkzVG'),
-    ('Sami','Samara', 'samara@example.com', '$2a$10$8yaLfN1mVcQZcMis149VZesprNumE0ULCaxKvM2P8mXuZ5eWTkzVG');
+-- Insert user data
+INSERT INTO users (username, name, email, password, user_type_id, verified) VALUES
+    ('silsil', 'Silvio', 'silvio@example.com', '$2a$10$8yaLfN1mVcQZcMis149VZesprNumE0ULCaxKvM2P8mXuZ5eWTkzVG', 1, TRUE), 
+    ('Tony', 'Antônio', 'tony@example.com', '$2a$10$8yaLfN1mVcQZcMis149VZesprNumE0ULCaxKvM2P8mXuZ5eWTkzVG', 1, TRUE), 
+    ('Lu', 'Luciana', 'luciana@example.com', '$2a$10$8yaLfN1mVcQZcMis149VZesprNumE0ULCaxKvM2P8mXuZ5eWTkzVG', 2, FALSE),  
+    ('Sami', 'Samara', 'samara@example.com', '$2a$10$8yaLfN1mVcQZcMis149VZesprNumE0ULCaxKvM2P8mXuZ5eWTkzVG', 2, FALSE);
 
-
-
+-- Create auth_tokens table
 CREATE TABLE auth_tokens (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     token VARCHAR(100) NOT NULL,
     expires_at TIMESTAMP NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-
+-- Create profile table
 CREATE TABLE profile (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
@@ -38,20 +56,21 @@ CREATE TABLE profile (
     birthdate DATE,
     company VARCHAR(100),
     gender VARCHAR(10),
+    about TEXT, 
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP,
     deleted_at TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-
-INSERT INTO profile (user_id, avatar, birthdate, company, gender, created_at)
+-- Insert profile data
+INSERT INTO profile (user_id, avatar, birthdate, company, gender, about, created_at)
 VALUES
-    (1, 'https://avatars.githubusercontent.com/ViniciusQueiros16', '1990-06-15', 'ABC Company', 'Male', NOW()),
-    (2, 'https://avatars.githubusercontent.com/ViniciusQueiros16', '1985-12-25', 'XYZ Corporation', 'Female', NOW()),
-    (3, 'https://avatars.githubusercontent.com/ViniciusQueiros16', '1998-03-10', 'Sample Corp', 'Other', NOW());
+    (1, 'https://avatars.githubusercontent.com/ViniciusQueiros16', '1990-06-15', 'ABC Company', 'Male', 'About Silvio...', NOW()),
+    (2, 'https://avatars.githubusercontent.com/ViniciusQueiros16', '1985-12-25', 'XYZ Corporation', 'Female', 'About Antônio...', NOW()),
+    (3, 'https://avatars.githubusercontent.com/ViniciusQueiros16', '1998-03-10', 'Sample Corp', 'Other', 'About Luciana...', NOW());
 
-
+-- Create posts table
 CREATE TABLE posts (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
@@ -59,10 +78,10 @@ CREATE TABLE posts (
     image_url VARCHAR(255),
     communityType VARCHAR(50),
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
 );
 
-
+-- Insert post data
 INSERT INTO posts (user_id, message, image_url, communityType)
 VALUES
     (1, 'Primeiro post! #início',
@@ -71,3 +90,12 @@ VALUES
     (3, 'Compartilhando uma foto incrível.', 'https://www.istockphoto.com/br/foto/nas-asas-da-liberdade-p%C3%A1ssaros-que-voam-e-correntes-quebradas-conceito-da-carga-gm1141549703-305881902', 'Twitter'),
     (4, 'Explorando lugares novos.', 'https://avatars.githubusercontent.com/ViniciusQueiros16', 'AnyOne');
 
+-- Create matches table
+CREATE TABLE matches (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    matched_user_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP
+);
